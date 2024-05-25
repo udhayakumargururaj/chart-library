a83e9e88d681194836e5475c232aae64d92313f4
udhayakumar gururaj <udhayakumargururaj@gmail.com>
2024-05-26 00:12:50 +0530

    initial commit for chart lib

---
+++ packages/chart-library/src/components/LineChart.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    LineController,
+    BarController,
+  } from 'chart.js';
+  import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    LineController,
+    BarController
+  );
+
+const LineChart = ({ data, options, type }) => {
+  return <Chart type={type} data={data} options={options} />;
+};
+
+export default LineChart;
---
+++ packages/chart-library/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'chart-library.js',
+    library: 'ChartLibrary',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this',
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
